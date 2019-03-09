df755b5ab6df3d9dd62ccf21aca7e9456791294a
Kudo <kudo@example.com>
2019-03-09 18:46:53 +0900

    first commit

---
+++ fruits.py
@@ -0,0 +1,22 @@
+# 辞書を定義
+fruits_dict= {"りんご":"Apple",
+              "ぶどう":"Grape",
+              "みかん":"Orange"}
+
+# print(fruits["りんご"])
+# print(fruits["ぶどう"])
+# print(fruits["みかん"])
+
+# Keyだけforループ
+for jp_name in fruits_dict.keys():
+    print(jp_name)
+
+# Valueだけforループ
+for en_name in fruits_dict.values():
+    print(en_name)
+
+# KeyとValueを同時にforループ
+for jp_name, en_name in fruits_dict.items():
+    print(f"{jp_name}は英語で{en_name}")
+
+
